9fe6842dcac1bde640e0a5be308d66eb7f118dce
AhmadNk <51542349+208090@users.noreply.github.com>
2022-01-22 17:56:44 +0330

    Delete book directory

--- book/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Station)
-admin.site.register(Train)
-admin.site.register(Schedule)
-admin.site.register(Seat_Chart)
-admin.site.register(Ticket          )
